e71a68d8de51d17b3720428516de019626781d68
FikFikk <fikri225456@gmail.com>
2025-09-25 08:31:06 +0700

    update constatns baseurl

--- apps/server/src/config/constants.ts
+++ apps/server/src/config/constants.ts
@@ -1,6 +1,7 @@
 // src/config/constants.ts
 export const CONSTANTS = {
   PHP_BACKEND_URL: process.env.PHP_BACKEND_URL || 'https://admin-minicrm.jasamobileapp.com/api',
+  // PHP_BACKEND_URL: process.env.PHP_BACKEND_URL || 'http://localhost:8080/api',
   PHP_KEY: process.env.PHP_KEY || 'crm_e54c761e1dec987a98a0fdb593ff95dbd2fe5813',
   PORT: process.env.PORT || 3001,
   RECONNECT_DELAY: 5000,
