6940f4f7067ef0fe4a0b9b589be0a2d06308e15b
FikFikk <fikri225456@gmail.com>
2025-09-25 09:52:20 +0700

    update cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -11,9 +11,23 @@ import { Logger } from './utils/logger';
 
 const app = express();
 const server = createServer(app);
+
+// Debug CORS origins
+console.log('[SERVER DEBUG] CORS_ORIGINS config:', CONSTANTS.CORS_ORIGINS);
+
 const io = new SocketIOServer(server, {
   cors: {
-    origin: CONSTANTS.CORS_ORIGINS,
+    origin: function(origin, callback) {
+      console.log('[SOCKETIO CORS DEBUG] Incoming Origin:', origin);
+      console.log('[SOCKETIO CORS DEBUG] Allowed Origins:', CONSTANTS.CORS_ORIGINS);
+      if (!origin || CONSTANTS.CORS_ORIGINS.includes(origin)) {
+        console.log('[SOCKETIO CORS DEBUG] Origin ALLOWED');
+        callback(null, true);
+      } else {
+        console.log('[SOCKETIO CORS DEBUG] Origin REJECTED');
+        callback(new Error('Not allowed by CORS: ' + origin));
+      }
+    },
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -23,6 +37,16 @@ const io = new SocketIOServer(server, {
 app.use(corsMiddleware);
 app.use(express.json());
 app.use(loggingMiddleware);
+
+// Handle preflight requests specifically for socket.io
+app.options('/socket.io/*', (req, res) => {
+  console.log('[OPTIONS DEBUG] Preflight request for socket.io from:', req.headers.origin);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,x-api-key');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 
 // Setup Socket.IO
 setupSocketIO(io);
